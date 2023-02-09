3b2422b429b179bd1c70fc8d7ee11813ff1e3645
Sunjii <sophieadenn@gmail.com>
2023-02-09 09:48:20 +0900

    [fix]add scrollBehavior for mouse, touch, etc,..

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:toonflix/screen/home_screen.dart';
 
@@ -12,6 +14,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      scrollBehavior: const MaterialScrollBehavior().copyWith(
+        dragDevices: {
+          PointerDeviceKind.mouse,
+          PointerDeviceKind.touch,
+          PointerDeviceKind.stylus,
+          PointerDeviceKind.unknown
+        },
+      ),
       home: Container(
         child: HomeScreen(),
       ),
